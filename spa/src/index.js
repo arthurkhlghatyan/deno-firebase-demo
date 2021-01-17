1dfdfafbb073a98da9257c63f6a5930303a5859a
Arthur Khlghatyan <arthur.khlghatyan@gmail.com>
2021-01-17 14:38:35 +0400

    Added semantic-ui and react-router-dom

--- spa/src/index.js
+++ spa/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
